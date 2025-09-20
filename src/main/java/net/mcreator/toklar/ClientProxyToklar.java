cda0c40890a66548763bdb39a714607de5b2b10c
ToklarAkara <154789737+ToklarAkara@users.noreply.github.com>
2025-09-19 23:42:12 -0500

    Update ClientProxyToklar.java
    register candle model

--- src/main/java/net/mcreator/toklar/ClientProxyToklar.java
+++ src/main/java/net/mcreator/toklar/ClientProxyToklar.java
@@ -6,29 +6,39 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.mcreator.toklar.item.ItemSummonClanky;
 import net.mcreator.toklar.item.ItemSummonClankyElement;
+import net.mcreator.toklar.init.ModBlocks;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.model.obj.OBJLoader;
+import net.minecraft.item.Item;
 
 public class ClientProxyToklar implements IProxyToklar {
-	@Override
-	public void init(FMLInitializationEvent event) {
-	    ModelLoader.setCustomModelResourceLocation(
-	        ItemSummonClankyElement.block, 0,
-	        new ModelResourceLocation("toklar:ghastly_potion", "inventory")
-	    );
-	}
 
-	@Override
-	public void preInit(FMLPreInitializationEvent event) {
-		OBJLoader.INSTANCE.addDomain("toklar");
-	}
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        OBJLoader.INSTANCE.addDomain("toklar");
 
-	@Override
-	public void postInit(FMLPostInitializationEvent event) {
-	}
+        // Register Monster Candle item model
+        ModelLoader.setCustomModelResourceLocation(
+            Item.getItemFromBlock(ModBlocks.MONSTER_CANDLE),
+            0,
+            new ModelResourceLocation("toklar:monster_candle", "inventory")
+        );
+    }
 
-	@Override
-	public void serverLoad(FMLServerStartingEvent event) {
-	}
+    @Override
+    public void init(FMLInitializationEvent event) {
+        ModelLoader.setCustomModelResourceLocation(
+            ItemSummonClankyElement.block, 0,
+            new ModelResourceLocation("toklar:ghastly_potion", "inventory")
+        );
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+
+    @Override
+    public void serverLoad(FMLServerStartingEvent event) {
+    }
 }
